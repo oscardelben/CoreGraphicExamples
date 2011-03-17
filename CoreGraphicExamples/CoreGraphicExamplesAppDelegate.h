6f537a2d905df4c570adb093f28425a871b4ac2f
Oscar Del Ben <info@oscardelben.com>
2011-03-17 13:15:13 +0100

    Initial Commit

---
+++ CoreGraphicExamples/CoreGraphicExamplesAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  CoreGraphicExamplesAppDelegate.h
+//  CoreGraphicExamples
+//
+//  Created by Oscar Del Ben on 3/17/11.
+//  Copyright 2011 DibiStore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoreGraphicExamplesAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ CoreGraphicExamples/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  CoreGraphicExamples
+//
+//  Created by Oscar Del Ben on 3/17/11.
+//  Copyright 2011 DibiStore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
